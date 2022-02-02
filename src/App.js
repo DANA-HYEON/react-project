85a80947f3c19f281eb67531e7c2f9149ea80be9
Dahyeon Lee <dada4202@gmail.com>
2022-02-02 21:09:25 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./Button";
+import styles from "./App.module.css";
+import {useState, useEffect} from "react";
+
 
 function App() {
+  const [value, setValue] = useState("");
+  const [toDos, setTodos] = useState([]);
+
+  const onChange = (event)=>{
+    setValue(event.target.value);
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(value === ""){
+      return;
+    }
+
+    setTodos((prev)=>[...prev,value]);
+    setValue("");
+  }
+
+  const deleteBtn = (index) => {
+    setTodos(toDos.filter((item,todoIndex) => index !== todoIndex));
+  }
+
+  console.log(toDos);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My To Dos({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input value={value} onChange={onChange} type="text" placeholder="plz input your todo list"/>
+        <button>Add To Do</button>
+      </form>
+      <hr/>
+      <ul>
+        {toDos.map((item,index) => 
+          <li key={index}>{item} 
+          <button onClick={()=>deleteBtn(index)}>❌</button></li>)}
+      </ul>
     </div>
   );
 }
